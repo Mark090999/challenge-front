12c75adfa38698b33691f4704e2880669ac1b784
Mark <ccmarco090999@gmail.com>
2022-03-14 20:06:10 -0500

    Add vue-router dependency

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 2 + Vite" />
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-export default {
-  components: {
-    HelloWorld,
-  },
-};
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default {
+  name: 'App',
 }
-</style>
+
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Containers
+//const TheContainer = () => import('../containers/TheContainer')
+
+
+// Views - Pages
+//const Page404 = () => import('../views/pages/Page404')
+const Main = () => import('../views/Test.vue')
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history', // https://router.vuejs.org/api/#mode
+  linkActiveClass: 'active',
+  scrollBehavior: () => ({ y: 0 }),
+  routes: configRoutes(),
+})
+  
+function configRoutes () {  
+  return [    
+    {
+      path: '/',
+      name: 'Main',
+      component: Main,
+      meta: {
+        permission: 'public'
+      }
+    }
+  ]
+}
+ 
+
+export default router
