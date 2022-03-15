3b53c96b4b44cf51b4336fa8745f6bbe07766829
Mark <ccmarco090999@gmail.com>
2022-03-15 10:22:36 -0500

    Add main search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,41 @@
 <template>
   <b-container class="bv-example-row" align-v="center" fluid>
       <b-row class="buscador">
-        <b-col></b-col>
-        <b-col></b-col>
-        <b-col></b-col>
+        <div>
+          <b-card id="principal" title="Vuelos" sub-title="">
+            <b-card-body>
+              <b-container class="bv-example-row">
+                <b-row>
+                  <b-col>                    
+                    <label for="ori" style="font-size: 14px;">Ciudad de origen y destino</label>
+                    <b-input-group size="md">
+                      <b-form-input list="my-cities" id="ori" placeholder="Origen"></b-form-input>
+                      <b-form-input list="my-cities" placeholder="Destino"></b-form-input>
+                    </b-input-group>
+                  </b-col>
+                  <b-col>
+                    <label for="ori" style="font-size: 14px;">Fecha de Salida y retorno</label>
+                    <b-input-group size="md">
+                      <b-form-input placeholder="Salida" type="date"></b-form-input>
+                      <b-form-input placeholder="Retorno" type="date"></b-form-input>
+                    </b-input-group>
+                  </b-col>
+                  <b-col>   
+                    <label for="ori" style="font-size: 14px;"></label>
+                    <b-input-group size="md">                 
+                    <b-button variant="primary" size="md" class="mb-2">
+                      <b-icon icon="search" aria-hidden="true"></b-icon> Buscar
+                    </b-button>
+                    </b-input-group>
+                  </b-col>
+                </b-row>
+              </b-container>      
+              <datalist id="my-cities">
+                <option v-for="city in cities" :key="city">{{ city }}</option>
+              </datalist>        
+            </b-card-body>
+          </b-card>
+        </div>
       </b-row>
   </b-container>
 </template>
@@ -15,16 +47,20 @@ export default {
   },
   data() {
     return {
-      count: 0,
-      selected: null,
-        options: [
-          { value: 'A', text: 'Option A (from options prop)' },
-          { value: 'B', text: 'Option B (from options prop)' }
-        ]
+      cities: ['Quito', 'Loja', 'Macara']
     };
   },
 };
 </script>
 
 <style scoped>
+.buscador{
+  background-color: #f8f3f3fa;
+}
+#principal{
+  width: 80%; 
+  margin: auto;
+  background-color: #270570;
+  color: white;
+}
 </style>
